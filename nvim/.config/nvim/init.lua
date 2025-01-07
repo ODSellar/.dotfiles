2506857877fc7db56dbc4ee0e21b6631938a6c91
ollie <odsellar@gmail.com>
2025-01-07 20:15:23 +0000

    truncate telescope file names

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -381,11 +381,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          path_display = {
+            truncate = 3,
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
